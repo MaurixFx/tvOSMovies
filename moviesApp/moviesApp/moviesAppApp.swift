2f3ef57f0248f09b61d063ae9f20ab8981c2023b
Mauricio Figueroa <maurixfx@gmail.com>
2024-03-17 10:41:16 +0100

    Project init setup

---
+++ moviesApp/moviesApp/moviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesAppApp.swift
+//  moviesApp
+//
+//  Created by Mauricio Figueroa on 17-03-24.
+//
+
+import SwiftUI
+
+@main
+struct moviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
